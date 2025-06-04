601422fd20b657184b61a5126127c72e5d7e4bf0
luisejv <luis.jauregui@utec.edu.pe>
2025-06-03 22:33:30 -0500

    feat: trying husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,7 +21,43 @@ const eslintConfig = [
       "**/generated/**",
     ],
   },
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended"
+  ),
+  {
+    rules: {
+      "no-console": "warn",
+      "no-debugger": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      // Accesibilidad
+      "jsx-a11y/alt-text": "warn",
+      "jsx-a11y/anchor-is-valid": "warn",
+
+      // Estilo
+      quotes: ["warn", "double", { avoidEscape: true }],
+      semi: ["warn", "always"],
+      "comma-dangle": ["warn", "always-multiline"],
+      "object-curly-spacing": ["warn", "always"],
+
+      // Next.js
+      "@next/next/no-html-link-for-pages": "off",
+
+      // TypeScript
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/ban-ts-comment": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
