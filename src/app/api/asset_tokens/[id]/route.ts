171b65b5e4c16155de063823d690e6ffc36322a1
luisejv <luis.jauregui@utec.edu.pe>
2025-06-03 22:16:13 -0500

    fix: lint errors

--- src/app/api/asset_tokens/[id]/route.ts
+++ src/app/api/asset_tokens/[id]/route.ts
@@ -1,10 +1,7 @@
 import { createClient } from "@/utils/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: NextRequest) {
   try {
     const supabase = await createClient();
 
