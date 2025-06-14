c316bafbae167d66b6d581fb469d07a5c1d34595
vbuselli <vbuselli@berkeley.edu>
2025-06-14 14:55:30 -0700

    bug fix, no te lleva al checkout

--- src/components/AssetTokenCard.tsx
+++ src/components/AssetTokenCard.tsx
@@ -90,7 +90,7 @@ export default function AssetTokenCard(assetToken: AssetTokenCardProps) {
                   return;
                 }
                 router.push(
-                  `/protected/checkout`
+                  `/protected/checkout/${assetToken.id}`
                 );
               }}
             >
