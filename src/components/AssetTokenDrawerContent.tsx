a6b6472a2333526892b67ed8225a9f092962fabd
seo-onion <sebastianhernandezminano@gmail.com>
2025-07-15 14:13:15 -0500

    final push

--- src/components/AssetTokenDrawerContent.tsx
+++ src/components/AssetTokenDrawerContent.tsx
@@ -276,7 +276,7 @@ const AssetTokenDrawerContent: React.FC<Props> = ({
                           proyecto_id: assetToken.asset.id,
                           user_id: user?.id,
                         });
-                        handleValidationClick
+                        handleValidationClick();
                       }}
                       className="bg-primary text-white px-4 py-2 rounded mb-4 w-full cursor-pointer"
                     >
